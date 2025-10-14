c46cea281885bc2504f0e0da75fba8b8048841b7
AESiR <cyborgkiller1008@gmail.com>
2025-10-14 19:30:31 +0530

    Update venture names and add new venture 'Attracted By Jaayvee' with logo and URL to ventures list

--- lib/ventures.ts
+++ lib/ventures.ts
@@ -7,9 +7,11 @@ export type Venture = {
 };
 
 export const ventures: Venture[] = [
-  { id: 'talaash', name: 'Talaash', logoUrl: '/static/png with tagline white/talaash logo with tagline-03.png', internalPath: '/ease/talaash' },
-  { id: 'tours and travels', name: 'Tours and Travels', logoUrl: '/static/png with tagline white/travel and tours-03.png', href: 'https://toursandtravels.thejaayveeworld.com' },
-  { id: 'clothing', name: 'Clothing', logoUrl: '/static/png with tagline white/clothing-03.png', href: 'https://clothing.thejaayveeworld.com' },
-  { id: 'realestate', name: 'Real Estate', logoUrl: '/static/png with tagline white/real estate-03.png', href: 'https://realestate.thejaayveeworld.com' },
-  { id: 'events', name: 'Events', logoUrl: '/static/png with tagline white/event planner-03.png', href: 'https://events.thejaayveeworld.com' },
+  { id: 'talaash', name: 'Talaash By Jaayvee', logoUrl: '/static/png with tagline white/talaash logo with tagline-03.png', internalPath: '/ease/talaash' },
+  { id: 'tours and travels', name: 'Travel and Tour Operators', logoUrl: '/static/png with tagline white/travel and tours-03.png', href: 'https://travel.thejaayveeworld.com' },
+  { id: 'clothing', name: 'Clothing By Jaayvee', logoUrl: '/static/png with tagline white/clothing-03.png', href: 'https://clothing.thejaayveeworld.com' },
+  { id: 'realestate', name: 'Real Estate By Jaayvee', logoUrl: '/static/png with tagline white/real estate-03.png', href: 'https://realestate.thejaayveeworld.com' },
+  { id: 'events', name: 'Events Planners By Jaayvee', logoUrl: '/static/png with tagline white/event planner-03.png', href: 'https://events.thejaayveeworld.com' },
+  { id: 'atrracted', name: 'Attracted By Jaayvee', logoUrl: '/static/png with tagline white/clothing-03.png', href: 'https://attracted.thejaayveeworld.com' },
+
 ];
